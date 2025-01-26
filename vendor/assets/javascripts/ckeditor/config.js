3d0e0a9d44b58224dc6a219106618eec0d8ce11f
Tim Robinson <tbr00@pacbell.net>
2025-01-26 07:53:31 -0800

    remove vendor config

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -3,8 +3,11 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+/*
+ * remove to avoid Heroku conflict with application specific config
+ *CKEDITOR.editorConfig = function( config ) {
+ *	// Define changes to default configuration here. For example:
+ *	// config.language = 'fr';
+ *	// config.uiColor = '#AADC6E';
+ *};
+ */
