19f1afb61ee52907f145d4d6eb0b81f08f768972
Tim Robinson <tbr00@pacbell.net>
2025-01-26 17:54:28 -0800

    add custom configuration

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -3,11 +3,29 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/*
- * remove to avoid Heroku conflict with application specific config
- *CKEDITOR.editorConfig = function( config ) {
- *	// Define changes to default configuration here. For example:
- *	// config.language = 'fr';
- *	// config.uiColor = '#AADC6E';
- *};
- */
+CKEDITOR.editorConfig = function (config) {
+    config.language="en";
+    config.language="en";
+    config.removePlugins = 'exportpdf'
+    config.resize_dir = 'both';
+    config.width = '800'
+    config.fontSize_defaultLabel = '14px';
+    config.toolbar_mini = [
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'Ju\
+stifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+        { name: 'insert', items: [ 'Table', 'HorizontalRule','SpecialChar' ] },
+        { name: 'edit', items: ['Undo', 'Redo'] }
+    ];
+    config.specialChars = ['‸','°','⨁','✓','æ','±','×','÷','†','‡',
+                           '∓','∴','↑','↟','↓','↡',
+                           'Α','Β','Γ','Δ','Ε','Ζ','Η','Θ','Ι','Κ','Λ','Μ',
+                           'Ν','Ξ','Ο','Π','Ρ','Σ','Τ','Υ','Φ','Χ','Ψ','Ω',
+                           'α','β','γ','δ','ε','ζ','η','θ','ι','κ','λ','μ',
+                           'ν','ξ','ο','π','ρ','ς','σ','τ','υ','φ','χ','ψ','ω'];
+
+
+    config.toolbar='mini';
+    config.disableNativeSpellChecker = false;
+};
